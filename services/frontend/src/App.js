fb8493f73d1b28fbfc6bd7287ddb9394526c2895
Stakosh <soto.javi1@gmail.com>
2024-04-26 15:45:53 -0400

    weas con errores

--- services/frontend/src/App.js
+++ services/frontend/src/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout.js';
 import Inicio from './components/Inicio.js';
 import Noticias from './components/Noticias';
 import Login from './components/Login';
+import ContactList from './ContactList.jsx';
+
 
 
 
 function App() {
+  const [contacts, setContacts] = useState([])
+
+  useEffect(() => {
+    fetchContacts()
+  }, [])
+
+  const fetchContacts = async () => {
+    const response = await fetch("http://127.0.0.1:5000/contacts")
+    const data = await response.json()
+    setContacts(data.contacts)
+    console.log(data.contacts)
+  }
+
+
+
   return (
     <Router>
       <div className="App">
