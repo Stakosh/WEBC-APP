d1de919d1c2723a674d9a68bb223d58a4f344c3b
Stakosh <soto.javi1@gmail.com>
2024-04-28 15:33:06 -0400

    se ve el backend :D y ejecuta bien, respaldo

--- services/backend/main.py
+++ services/backend/main.py
@@ -79,8 +79,7 @@ def initialize_default_user():
 
 
 
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
+with app.app_context():
+    db.create_all()
 
     app.run(debug=True)
